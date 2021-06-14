ed4f0d1bd90ecb672cdf1599f17c30af9d5ac09e
ahmadazharrii <ahmadazhaarrii@gmail.com>
2021-06-14 16:50:58 +0700

    menambah menu header overlay yang tidak bisa di scroll kebawah

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,27 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
+const fadeElement = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){ //close hamburger menu
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        body.classList.remove('noscroll');
+        header.classList.remove('open');        
+        fadeElement.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+
     }
     else { //open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElement.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        }); 
     }
 });
